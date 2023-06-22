006fec5aeda34cb0142673d0fffa7a4698d01d37
Agustin Sigales <asea301998@gmail.com>
2023-06-22 15:12:05 -0300

    last commit

--- client/src/components/navbar/navbar.component.jsx
+++ client/src/components/navbar/navbar.component.jsx
@@ -5,19 +5,32 @@ import {Link} from "react-router-dom"
 function Navbar({ handleChange, handleSubmit, allGenres, handleFilterGenre }) {
   return (
     <div className={styles.Navbar}>
-      <form onChange={handleChange}>
-        <input className={styles.NavbarInput} type="search" />
-        <button className={styles.NavbarButton} type="submit" onClick={handleSubmit}>
-          Buscar
-        </button>
-        <button className={styles.CrearButton}>
-        <Link  to={`/create`}>Crear Juego</Link>
-        </button>
-        <select className={styles.NavbarButton} onChange={e => handleFilterGenre(e)} >
-      {allGenres?.map((genre,index)=>(
-        <option key={index} value={genre.name}>{genre.name}</option>
-      ))}
-      </select>
+      <form className={styles.NavbarForm} onChange={handleChange}>
+        <div className={styles.NavbarSearch}>
+          <input className={styles.NavbarInput} type="search" />
+          <button
+            className={styles.NavbarButton}
+            type="submit"
+            onClick={handleSubmit}
+          >
+            Buscar
+          </button>
+          <button className={styles.CrearButton}>
+            <Link to={`/create`}>Crear Juego</Link>
+          </button>
+        </div>
+        <div className={styles.NavbarFilter}>
+          <select
+            className={styles.NavbarButton}
+            onChange={(e) => handleFilterGenre(e)}
+          >
+            {allGenres?.map((genre, index) => (
+              <option key={index} value={genre.name}>
+                {genre.name}
+              </option>
+            ))}
+          </select>
+        </div>
       </form>
     </div>
   );
